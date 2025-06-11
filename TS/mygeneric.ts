fd88417f7d6e8e8cd45548a42562040cba6bdc8b
Sundarleo11 <sundarrock1999@gamil.com>
2025-06-11 08:52:19 +0530

    generic function example

---
+++ TS/mygeneric.ts
@@ -0,0 +1,18 @@
+const score: Array<number> = [];
+const Name: Array<string> = [];
+
+function addscore(val: number | string): Number | string {
+  return val;
+}
+
+function addscore1(val: any): any {
+  return val;
+}
+
+function addscore2<T>(val: T): T {
+  return val;
+}
+
+function addscore3<Type>(val: Type): Type {
+  return val;
+}
